3a0aad578e93bfbaecd58a99890dd779ed2a6cd4
licwan62 <161543834+licwan62@users.noreply.github.com>
2024-04-03 13:19:56 +1300

    Add files via upload

---
+++ direct.js
@@ -0,0 +1,10 @@
+var o = document.body;
+window.onload = function() {
+	createDIV();
+}
+
+function createDIV() {
+	var cell = document.createElement('div')
+	cell.innerHTML = "<div><a href="+"../../index.html"+">回到主页</a></div>"
+	o.append(cell)
+}
